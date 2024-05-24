a3d5ece8f18befcc8dcd024e5854098115b7b002
aimunNaharr <aimunnaharr@gmail.com>
2024-05-24 19:13:35 +0600

    updated

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
+import { motion } from "framer-motion";
 import React from "react";
-import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 type TLayout = {
   children: React.ReactNode;
@@ -10,7 +11,20 @@ export default function Layout({ children }: TLayout) {
   return (
     <div className="relative">
       <Navbar />
-      <div className="min-h-screen">{children}</div>
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: {
+            duration: 1,
+            type: "spring",
+          },
+        }}
+        className="min-h-screen"
+      >
+        {children}
+      </motion.div>
       <Footer />
     </div>
   );
